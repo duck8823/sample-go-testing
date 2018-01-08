26db2e6698f0828d4fd473377091d80164e0a29c
shunsuke maeda <duck8823@gmail.com>
2018-01-08 21:47:33 +0900

    Add test case using httptest

--- server/server_test.go
+++ server/server_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/duck8823/sample-go-testing/server/mock_server"
 	"github.com/golang/mock/gomock"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -20,6 +21,23 @@ func Test_RoutingWithStartServer(t *testing.T) {
 	}()
 
 	_, err := http.Get("http://localhost:1234/hello")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_RoutingWitHttpTest(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockController := mock_server.NewMockController(ctrl)
+	mockController.EXPECT().Get(gomock.Any()).Times(1)
+
+	s := Create(mockController)
+	server := httptest.NewServer(s)
+	client := server.Client()
+
+	_, err := client.Get("http://localhost:1234/hello")
 	if err != nil {
 		t.Fatal(err)
 	}
