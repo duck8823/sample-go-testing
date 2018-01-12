5d462471142ebebe6ab066a060804a42b3faca79
shunsuke maeda <duck8823@gmail.com>
2018-01-12 09:13:13 +0900

    Fix main.go

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 
 func main() {
 	optC := app.ParseFlag(os.Args[1:]...)
-	fmt.Printf("option c: %s", optC)
+	fmt.Printf("option c: %s\n", optC)
 
-	duck := &animals.Duck{}
-	apple := &foods.Apple{}
+	duck := &animals.Duck{"tarou"}
+	apple := foods.NewApple("sunfuji")
 	println(duck.Eat(apple))
 }
